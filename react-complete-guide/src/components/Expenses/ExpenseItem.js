7cfe4e985054574c17d18787b8c72485c7886f92
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2021-07-14 16:58:14 +1000

    Experimented with States

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,24 @@
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import React, {useState} from "react";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  console.log('Evaluating ExpenseItem');
+  const clickHandler = ()=>{
+    setTitle('Updated!');
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2 className="expense-date__year">{props.title}</h2>
+        <h2 className="expense-date__year">{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
