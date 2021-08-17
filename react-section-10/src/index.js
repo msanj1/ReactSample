ef75a7aa5b56484a7f92d1b6c891074c61d4cbb4
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2021-08-18 08:13:30 +1000

    Used the Context Api and forwardrefs

--- react-section-10/src/index.js
+++ react-section-10/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { AuthContextProvider } from './components/store/auth-context';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AuthContextProvider>
+    <App></App>
+</AuthContextProvider>, document.getElementById('root'));
